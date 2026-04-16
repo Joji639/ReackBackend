fa4c35abedde2f285e989bdb63b4302ef7a188cd
joji639 <jojijoj367@gmail.com>
2026-04-16 16:57:01 +0530

    fix superuser

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-set -o errexit
-
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+email='admin@gmail.com';
+password='admin123';
+
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(
+        email=email,
+        password=password
+    )
+"
