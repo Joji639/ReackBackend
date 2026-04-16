8cc78f4c0d5dba337855bba11085bbbf0d79655a
joji639 <jojijoj367@gmail.com>
2026-04-16 18:14:52 +0530

    updated version

--- build.sh
+++ build.sh
@@ -9,10 +9,12 @@ from django.contrib.auth import get_user_model;
 User = get_user_model();
 email='Admin@gmail.com';
 password='@syntaxerror%';
+username='admin';
 
 if not User.objects.filter(email=email).exists():
     User.objects.create_superuser(
         email=email,
+        password=password
         password=password
     )
 "
