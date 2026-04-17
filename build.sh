34c2fe41349d26d3f0ae9c23694c46f06d5e839c
joji639 <jojijoj367@gmail.com>
2026-04-17 14:16:59 +0530

    remove duplicate admin creation

--- build.sh
+++ build.sh
@@ -8,19 +8,3 @@ python manage.py migrate
 
 echo "🔥 Loading data..."
 python manage.py loaddata data.json
-
-echo "🔥 Creating admin..."
-python manage.py shell -c "
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-email='admin@gmail.com';
-password='admin123';
-username='admin';
-
-if not User.objects.filter(email=email).exists():
-    User.objects.create_superuser(
-        email=email,
-        username=username,
-        password=password
-    )
-"
