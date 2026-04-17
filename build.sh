1135dd6c6b06f7c6f808233dfa909ddfa9dd8dd7
joji639 <jojijoj367@gmail.com>
2026-04-17 12:57:45 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
+set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 
+# 🔥 LOAD DATA (products + users)
+python manage.py loaddata data.json || true
+
+# 🔥 CREATE ADMIN (only if not exists)
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
