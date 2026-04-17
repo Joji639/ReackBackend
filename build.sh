1ac25fad908d12ea41b671038479be0eba45fa29
joji639 <jojijoj367@gmail.com>
2026-04-17 12:25:29 +0530

    add build script

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
-email='Adminfinal@gmail.com';
-password='@syntaxerror%1';
+email='admin@gmail.com';
+password='admin123';
 username='admin';
 
 if not User.objects.filter(email=email).exists():
